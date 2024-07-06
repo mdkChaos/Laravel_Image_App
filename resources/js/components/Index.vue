4c356cada94b0e80e618642508cd3a6720ce05eb
Andrii Poludniev <mdkchaos1@gmail.com>
2024-07-06 16:15:25 +0200

    Add Dropzone package

--- resources/js/components/Index.vue
+++ resources/js/components/Index.vue
@@ -1,13 +1,27 @@
 <script setup>
+import {onMounted, ref, nextTick} from 'vue';
+import Dropzone from 'dropzone';
 
+const dropzoneElement = ref(null);
+
+let dropzoneInstance = null;
+
+onMounted(async () => {
+    await nextTick();
+    dropzoneInstance = new Dropzone(dropzoneElement.value, {
+        url: "someUrl",
+    });
+});
 </script>
 
 <template>
-    <div>
+    <div class="container col-3">
         <h2>Index Vue</h2>
+        <div ref="dropzoneElement" class="p-5 d-block btn btn-dark">
+            Upload file
+        </div>
     </div>
 </template>
 
 <style scoped>
-
 </style>
