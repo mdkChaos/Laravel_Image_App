938bce5148c2689ca4f6f67bfb0b665882665a47
Andrii Poludniev <mdkchaos1@gmail.com>
2024-07-06 14:56:38 +0200

    add HomeController, view Index, model Image and Post

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import Index from "./components/Index.vue";
+
+
+const app = createApp(Index);
+
+app.mount('#app');
---
+++ resources/js/components/Index.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h2>Index Vue</h2>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
